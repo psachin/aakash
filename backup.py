7cf830e11e85cb45f3ae84ee21ee9f3daaf0e4bb
psachin <iclcoolster@gmail.com>
2012-03-15 19:45:35 +0530

    commented version

--- backup.py
+++ backup.py
@@ -1,41 +1,48 @@
 #!/usr/bin/python
+'''
+A simple script to handle GIT in daemon mode
+'''
 
 import os,time
-
 from subprocess import Popen, PIPE
-#eachfile = ''
 
+# function to handle newly untracked files
 def gitUntracked():
-    commit_msg='added'
-    untracked = Popen('git ls-files . --exclude-standard --others',shell=True, stdout=PIPE).stdout.readlines()
+    commit_msg='added'          # commit message, if require in case
+    # list all untracked files
+    untracked = Popen('git ls-files . --exclude-standard --others',shell=True, stdout=PIPE).stdout.readlines() 
+    # go through every file and add
     for untrackedfile in untracked:
         gitAdd(untrackedfile,commit_msg)
 
+# function to handle modification made to file(s)
 def gitModified():
     commit_msg='modified'
     modified = Popen('git ls-files --modified',shell=True, stdout=PIPE).stdout.readlines()
     for modifiedfile in modified:
         gitAdd(modifiedfile,commit_msg)
 
+# function to handle deleted file(s)
 def gitDelete():
     commit_msg='deleted'
     deleted = Popen('git ls-files --deleted',shell=True, stdout=PIPE).stdout.readlines()
     for deletedfile in deleted:
         gitdelete='git rm ' + deletedfile
         os.system(gitdelete)
-        #gitCommit(commit_msg)
 
+# function to show cached file(s)
 def gitCached():
     cached = Popen('git ls-files --cached',shell=True, stdout=PIPE).stdout.readlines()
     for cachedfile in cached:
         print cachedfile
 
+# function to add changes
 def gitAdd(x,msg):
     #print msg
     gitadd = 'git add ' + x
     os.system(gitadd)
-#    gitCommit(msg)
 
+# commit changes
 def gitCommit():
     #print "In function gitCommit " + msg
     myWeek = time.strftime("%a")
@@ -44,7 +51,8 @@ def gitCommit():
     timeStamp='Commit on: ' + myWeek + ' ' + myDate + ' ' + myTime
     gitcommit='git commit -m ' + "'" + timeStamp + ' ' +  "'" 
     os.system(gitcommit)
-    
+
+# check if git is initiated, if not initiate it
 def gitstart():
     # if not os.path.exists('.git'):
     if not os.path.isdir('.git'):
@@ -52,13 +60,15 @@ def gitstart():
         os.system('git init')       
     else:
         #print ".git/ dir exist"
+
+        # call functions
         gitUntracked()
         gitModified()
         gitDelete()
         gitCommit()
 
 
-        
+# go through inifinite loop, but wait for 15 sec before scanning any changes
 while True:
     gitstart() 
     time.sleep(15)
